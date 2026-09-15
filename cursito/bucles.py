32b3d2885713305ec5249117344612faaf27663b
OscarF2834 <pipesegoviaalvarado@gmail.com>
2026-09-14 21:29:22 -0500

    Bucles

---
+++ cursito/bucles.py
@@ -0,0 +1,11 @@
+numeros = [1,2,3,4,5]
+suma = 0
+
+for numero in numeros:
+    suma += numero
+    print("La suma de los numeros es de: " , suma)
+
+print("")
+
+for i in range(10):
+    print(i+1)
